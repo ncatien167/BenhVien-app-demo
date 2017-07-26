dc3bbdc6a4ebb7cf0a0ab1c5700f3558234983b3
ncatien167 <ncatien167@gmail.com>
2017-07-26 16:42:53 +0700

    login storyboard

---
+++ BenhVien/BenhVien/ViewController/UI/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  BenhVien
+//
+//  Created by AnhTien on 7/25/17.
+//  Copyright © 2017 AnhTien. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *txtHoTen;
+@property (weak, nonatomic) IBOutlet UITextField *txtEmail;
+@property (weak, nonatomic) IBOutlet UITextField *txtMatkhau;
+
+@end
